f8cbb90ba7c4966a09801a22ba422571d5a5e298
huangqiuhuang <hqh@hicustom.com>
2024-02-20 14:18:44 +0800

    feat: 工具函数

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,28 @@
-const sum = (a, b) => {
-  return a + b;
+export const loadJS = (url: string) => {
+  return new Promise((resolve, reject) => {
+    const script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = url;
+    document.getElementsByTagName('head')[0].appendChild(script);
+    //其他浏览器
+    script.onload = () => resolve('');
+    script.onerror = () => reject('加载错误');
+  });
 };
 
-export { sum };
+export const sleep = (timeout = 0) => new Promise((resolve) => setTimeout(resolve, timeout));
+
+export const getBase64 = (file: File) => {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = (error) => reject(error);
+  });
+};
+
+// 手机号脱敏
+export const mobileDesensitize = (mobile) => {
+  const reg = /(\d{3})\d*(\d{4})/;
+  return mobile.replace(reg, `$1****$2`);
+};
